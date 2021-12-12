913acd0199025ffe3959ad8ab88d194d2a92bf6e
korey-h <78871678+korey-h@users.noreply.github.com>
2021-12-12 23:57:37 +0500

    Update settings.py

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '^vk+@9!+eyst5!=jsvkjg1+hahw7emkg%&3pm!)shmeju#hygt'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['62.84.122.204', '127.0.0.1', 'localhost', 'backend']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'backend', '51.250.17.119']
 
 
 # Application definition
