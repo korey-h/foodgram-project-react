df91acb6739a72d71da8d28756350b5c8e2144ce
korey-h <78871678+korey-h@users.noreply.github.com>
2021-12-19 18:01:04 +0500

    Update settings.py

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '^vk+@9!+eyst5!=jsvkjg1+hahw7emkg%&3pm!)shmeju#hygt'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'backend', '91.217.80.6']
 
